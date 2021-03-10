6f24dfc1fb7891645c0ed84cd2896aa23144afae
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-03-10 10:40:15 +0900

    seeds.rbにテスト用ユーザーを作成できるように設定

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+EMAIL = 'test@example.com'
+PASSWORD = 'password'
+
+# テストユーザーが存在しないときだけ作成
+User.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  puts 'ユーザーの初期データインポートに成功しました。'
+end
