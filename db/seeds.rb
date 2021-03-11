477ea976856642e71a489ace8b978fd059d74a55
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-03-11 12:07:58 +0900

    seeds.rbに動画教材のcsvインポート機能を実装(エラー中)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
 
@@ -5,4 +7,14 @@ PASSWORD = 'password'
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
+end
+
+# 動画教材のcsvインポート
+Movie.destroy_all
+CSV.foreach('db/csv_data/movie_data.csv', headers: true) do |row|
+  Movie.create(
+    genre: row["genre"],
+    title: row["title"],
+    url: row["url"]
+  )
 end
