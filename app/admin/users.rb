47379b01e0d69a37ea3af3747ec36952a9a4c414
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-03-21 18:00:52 +0900

    app/admin/users.rbの不要なカラムを削除

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -21,15 +21,11 @@ ActiveAdmin.register User do
     selectable_column
     id_column
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
     column :created_at
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
   filter :created_at
 
   form do |f|
@@ -41,5 +37,4 @@ ActiveAdmin.register User do
     f.actions
   end
 
-  
 end
