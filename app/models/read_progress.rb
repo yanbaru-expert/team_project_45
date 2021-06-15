07e1d823548dedb326dbe8d84f787e1ce851fc5a
urayoshie <nof.q.p1417@gmail.com>
2021-06-15 17:20:49 +0900

    非同期処理とReadProgressモデルの日本語訳追加

--- app/models/read_progress.rb
+++ app/models/read_progress.rb
@@ -4,6 +4,6 @@ class ReadProgress < ApplicationRecord
 
   validates :user_id, uniqueness: {
                         scope: :text_id,
-                        message: "は同じ教材に2回以上読破済みとすることは出来ません",
+                        message: "は同じテキスト教材を2回以上読破済みにはできません",
                       }
 end
