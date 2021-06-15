5ec9e298600a5c8b96e23453772b6a90e3c5ad2b
urayoshie <nof.q.p1417@gmail.com>
2021-06-15 16:44:53 +0900

    読破済み機能の実装

--- app/models/read_progress.rb
+++ app/models/read_progress.rb
@@ -2,8 +2,8 @@ class ReadProgress < ApplicationRecord
   belongs_to :user
   belongs_to :text
 
-  validates :user_id, uniquness: {
+  validates :user_id, uniqueness: {
                         scope: :text_id,
-                        message: "は同じ教材に2回以上読破済みとすることは出来ません"、
+                        message: "は同じ教材に2回以上読破済みとすることは出来ません",
                       }
 end
