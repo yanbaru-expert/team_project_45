5348412c248116114a1f08b4d5b7067401b9c766
motokitakashima <pageboy.jr@icloud.com>
2021-06-28 15:58:55 +0900

    動画教材の一覧表示を実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "texts#index"
   get "/texts/:id", to: "texts#show"
+  get "/movies", to: "movies#index"
 
   resources :texts do
     resource :read_progresses, only: [:create, :destroy]
