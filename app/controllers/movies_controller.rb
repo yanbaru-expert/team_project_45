de9bf6890c4e3148a398bc6cb4e9087fe3b82d20
motokitakashima <pageboy.jr@icloud.com>
2021-07-07 20:39:55 +0900

    showアクションを削除

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,4 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST)
   end
-
-  def show
-    @movies = Movie.find(params[:id])
-  end
 end
