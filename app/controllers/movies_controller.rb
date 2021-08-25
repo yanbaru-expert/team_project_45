841b2aa0f88775d8d98724c5e39a5d2d300fd85b
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-08-25 22:46:31 +0900

    N+1問題を解消

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,9 @@ class MoviesController < ApplicationController
   def index
     @movies = 
     if params[:genre] == "php"
-      Movie.where(genre: Movie::PHP_GENRE_LIST)
+      Movie.includes(:watch_progresses).where(genre: Movie::PHP_GENRE_LIST)
     else
-      Movie.where(genre: Movie::RAILS_GENRE_LIST)
+      Movie.includes(:watch_progresses).where(genre: Movie::RAILS_GENRE_LIST)
     end
   end
 end
