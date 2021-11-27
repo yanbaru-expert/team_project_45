1fee8662d2021c6c5ad1fb863b4c170f20ef0dc5
akko-k <barcarolle.op60.horowitz@gmail.com>
2021-11-27 11:49:31 +0900

    textsコントローラのtextsテーブルのusers テーブルとの関連づけを削除

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,7 +1,7 @@
 class TextsController < ApplicationController
   def index
     select_sql = "texts.*, read_progresses.user_id = #{current_user.id} AS read"
-    @texts = Text.includes(:user).left_join_read_texts(current_user.id).select(select_sql).order(:created_at).filter_by(params[:genre])
+    @texts = Text.includes(:read_progresses).left_join_read_texts(current_user.id).select(select_sql).order(:created_at).filter_by(params[:genre])
   end
 
   def show
